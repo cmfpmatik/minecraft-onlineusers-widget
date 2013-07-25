6368830f014d600b3a726f58da60d49db1dfe9b4
pirmax <pirmax@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-25 01:38:35 +0000

    Grande mise à jour du plugin.
    git-svn-id: https://plugins.svn.wordpress.org/minecraft-onlineusers-widget/trunk@745983 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,16 +1,16 @@
 === Minecraft OnlineUsers Widget ===
 Contributors: pirmax
-Donate link: http://www.maxence-blog.fr/
+Donate link: http://pirmax.fr/
 Tags: minecraft, online, users, widget, user, player, display, affichage, extension, plugin, afficher
-Requires at least: 2.7
-Tested up to: 3.4.1
+Requires at least: 3.0
+Tested up to: 3.5.2
 Stable tag: trunk
 
-Plugin Widget permettant d'afficher les joueurs en ligne d'un serveur dans le menu du blog.
+Plugin Widget permettant d'afficher les joueurs en ligne d'un serveur Minecraft dans le menu d'un blog Wordpress.
 
 == Description ==
 
-Le plugin <b>Minecraft OnlineUsers Widget</b> est un plugin permettant d'afficher les joueurs en ligne d'un serveur dans le menu du blog grâce à la fonction "query" de CraftBukkit.
+Le plugin <b>Minecraft OnlineUsers Widget</b> est un plugin permettant d'afficher les joueurs en ligne d'un serveur dans le menu du blog grâce à la fonction "query" de CraftBukkit.Soutenez le créateur de cette extension en vous abonnant à sa chaîne : <a href="http://www.youtube.com/user/PirmaxLePoulpeRouge" target="_blank">PirmaxLePoulpeRouge</a>.
 
 == Installation ==
 
@@ -25,6 +25,10 @@ Rendez-vous dans le pannel des widgets sur votre blog Wordpress, et ajoutez le w
 1. screenshot-1.png 
 
 == Changelog ==
+
+= 1.2 =
+
+* Ajout d'une option pour modifier le CSS du Widget* Ajout d'une option pour modifier le texte écrit si aucun joueur est connecté* Possibilité de gérer la taille des avatars* Utilisation des avatars de Minotar.net* Résolution des erreurs PHP qui empêchaient le bon fonctionnement du plugin
 
 = 1.1 =
 
--- widget.php
+++ widget.php
@@ -39,14 +39,36 @@ class widget_mou extends WP_widget
 
 		$defaut = array(
 			"title" => "Les joueurs en ligne",
+			"ifNoPlayer" => "Aucun joueur en ligne",
 			"serverip" => "",
 			"serverport" => "25565",
 			"displayAvatar" => 1,
 			"displayCount" => 1,
 			"nbSlot" => 30,
+			"avatarSize" => 25,
+			"styleCSS" => "#mouw_li {
+	font-size: 15px;
+	font-weight: bold;
+}
+
+#mouw_li #mouw_avatar {
+	vertical-align: middle;
+	margin-right: 10px;
+}
+
+#mouw_title {
+	
+}
+
+#mouw_number {
+	margin-left: 20px;
+	font-weight: bold;
+}",
 			"format_data" => ""
 		);
 		$instance = wp_parse_args($instance, $defaut);
+
+		$GetPlayers = array();
 
 		if(empty($instance['serverip']) OR empty($instance['serverport']))
 		{
@@ -70,90 +92,77 @@ class widget_mou extends WP_widget
 
 			extract($args);
 
+			$displayWidget .= '<ul id="mouw_ul">';
+
 			if($GetPlayers !== false)
 			{
-				$displayWidget .= '<ul id="playerList">';
 				foreach ($GetPlayers as $i => $value)
 				{
 					if($instance['displayAvatar'] !== 1)
 					{
-						$displayWidget .= '<li style="font-size: 15px;"><img src="' . plugin_dir_url( __FILE__ ) . 'DisplayFace_player.php?pseudo=' . $value . '&size=25" width="25" height="25" border="0" title="" alt="" style="vertical-align: middle;" /> <strong>' . $value . '</strong></li>';
-					}
-					else
-					{
-						$displayWidget .= '<li style="font-size: 15px;"><strong>' . $value . '</strong></li>';
-					}
-				}
-				$displayWidget .= '</ul>';
-				$resnbPlayer = count($GetPlayers);
-			}
-			else
-			{
-				$displayWidget .= '<ul id="playerList">';
-				$displayWidget .= '<li>Aucun joueur en ligne</li>';
-				$displayWidget .= '</ul>';
-				$resnbPlayer = 0;
-			}
-
-			echo $before_widget;
-
-			if($instance['displayCount'] !== 1)
-			{
-				echo $before_title . $instance['title'] . '<strong style="margin-left: 20px;">' . $resnbPlayer . '/' . $instance['nbSlot'] . '</strong>' . $after_title;
-			}
-			else
-			{
-				echo $before_title . $instance['title'] . $after_title;
-			}
-
-			echo $displayWidget;
-			echo $after_widget;
-
-		}
-
-	}
-
-	function update($new, $old)
-	{
-		return $new;
-	}
-
-	function form($d)
-	{
-
+						$displayWidget .= '<li id="mouw_li"><img src="https://minotar.net/helm/' . $value . '/' . $instance['avatarSize'] . '.png" width="' . $instance['avatarSize'] . '" height="' . $instance['avatarSize'] . '" border="0" title="' . $value . '" alt="avatar_' . $value . '" id="mouw_avatar" />' . $value . '</li>';					}					else					{						$displayWidget .= '<li id="mouw_li">' . $value . '</li>';					}				}				$resnbPlayer = count($GetPlayers);			}			else			{				$displayWidget .= '<li id="mouw_li">' . $instance['ifNoPlayer'] . '</li>';				$resnbPlayer = 0;			}			$displayWidget .= '</ul>';			echo $before_widget;			if($instance['displayCount'] !== 1)			{				echo $before_title . '<span id="mouw_title">' . $instance['title'] . '</span><span id="mouw_number">' . $resnbPlayer . '/' . $instance['nbSlot'] . '</span>' . $after_title;			}			else			{				echo $before_title . $instance['title'] . $after_title;			}			echo $displayWidget;			echo $after_widget;		}	}	function update($new, $old)	{		return $new;	}	function form($d)	{
 		$defaut = array(
 			"title" => "Les joueurs en ligne",
+			"ifNoPlayer" => "Aucun joueur en ligne",
 			"serverip" => "",
 			"serverport" => "25565",
 			"displayAvatar" => 1,
 			"displayCount" => 1,
 			"nbSlot" => 30,
+			"avatarSize" => 25,
+			"styleCSS" => "#mouw_li {
+	font-size: 15px;
+	font-weight: bold;
+}
+
+#mouw_li #mouw_avatar {
+	vertical-align: middle;
+	margin-right: 10px;
+}
+
+#mouw_title {
+	
+}
+
+#mouw_number {
+	margin-left: 20px;
+	font-weight: bold;
+}",
 			"format_data" => ""
 		);
 		$d = wp_parse_args($d, $defaut);
 
 		?>
+		<?php if(!function_exists('fwrite')){ echo '<p style="border-bottom: 1px dashed #FF0000; color: #FF0000; padding-bottom: 5px;"><b>Attention!</b> La fonction PHP <code>fwrite()</code> n\'est pas disponible sur votre hébergement. Contactez votre administrateur système.</p>'; } ?>
 		<p style="border-bottom: 1px dashed #CCCCCC; padding-bottom: 5px;">
 		Pour activer le widget, vous devez activer <code>enable-query</code> (<b>enable-query=true</b>) dans le fichier <code>server.properties</code> de votre serveur CraftBukkit puis red&eacute;marrer votre serveur.
 		</p>
 		<p>
 		<label for="<?php echo $this->get_field_id('title'); ?>">Titre du widget :</label><br />
-		<input value="<?php echo $d['title']; ?>" name="<?php echo $this->get_field_name('title'); ?>" id="<?php echo $this->get_field_id('title'); ?>" type="text" size="100%" />
+		<input value="<?php echo $d['title']; ?>" name="<?php echo $this->get_field_name('title'); ?>" id="<?php echo $this->get_field_id('title'); ?>" type="text" size="35" style="margin-left: 1em;" />
 		</p>
 		<p>
 		<label for="<?php echo $this->get_field_id('serverip'); ?>">Adresse IP du serveur :</label><br />
-		<input value="<?php echo $d['serverip']; ?>" name="<?php echo $this->get_field_name('serverip'); ?>" id="<?php echo $this->get_field_id('serverip'); ?>" type="text" size="100%" /><br />
+		<input value="<?php echo $d['serverip']; ?>" name="<?php echo $this->get_field_name('serverip'); ?>" id="<?php echo $this->get_field_id('serverip'); ?>" type="text" size="35" style="margin-left: 1em;" /><br />
 		<label for="<?php echo $this->get_field_id('serverport'); ?>">Port du serveur :</label><br />
-		<input value="<?php echo $d['serverport']; ?>" name="<?php echo $this->get_field_name('serverport'); ?>" id="<?php echo $this->get_field_id('serverport'); ?>" type="text" size="100%" />
+		<input value="<?php echo $d['serverport']; ?>" name="<?php echo $this->get_field_name('serverport'); ?>" id="<?php echo $this->get_field_id('serverport'); ?>" type="text" size="20" style="margin-left: 1em;" />
 		</p>
 		<p>
 		<label for="<?php echo $this->get_field_id('nbSlot'); ?>">Nombre de slot du serveur :</label><br />
-		<input value="<?php echo $d['nbSlot']; ?>" name="<?php echo $this->get_field_name('nbSlot'); ?>" id="<?php echo $this->get_field_id('nbSlot'); ?>" type="text" size="100%" />
+		<input value="<?php echo $d['nbSlot']; ?>" name="<?php echo $this->get_field_name('nbSlot'); ?>" id="<?php echo $this->get_field_id('nbSlot'); ?>" type="text" size="10" style="margin-left: 1em;" /> slot(s)  <abbr title="Nombre de slot disponible sur votre serveur">(?)</abbr>
 		</p>
 		<p>
-		<label for="<?php echo $this->get_field_id('displayAvatar'); ?>"><input name="<?php echo $this->get_field_name('displayAvatar'); ?>" id="<?php echo $this->get_field_id('displayAvatar'); ?>" type="checkbox" <?php if($d['displayAvatar'] !== 1){ echo 'checked'; } ?> /> Afficher les avatars</label><br />
-		<label for="<?php echo $this->get_field_id('displayCount'); ?>"><input name="<?php echo $this->get_field_name('displayCount'); ?>" id="<?php echo $this->get_field_id('displayCount'); ?>" type="checkbox" <?php if($d['displayCount'] !== 1){ echo 'checked'; } ?> /> Afficher le nombre de joueur en ligne</label>
+		<label for="<?php echo $this->get_field_id('avatarSize'); ?>">Taille des avatars :</label><br />
+		<input value="<?php echo $d['avatarSize']; ?>" name="<?php echo $this->get_field_name('avatarSize'); ?>" id="<?php echo $this->get_field_id('avatarSize'); ?>" type="text" size="10" style="margin-left: 1em;" /> pixel(s)  <abbr title="Nombre de pixel (Longueur x Hauteur) de l'image">(?)</abbr>
 		</p>
+		<p id="editCSS">
+		<label for="<?php echo $this->get_field_id('styleCSS'); ?>">Modifier le style CSS :</label><br />
+		<textarea name="<?php echo $this->get_field_name('styleCSS'); ?>" id="<?php echo $this->get_field_id('styleCSS'); ?>" cols="36" rows="10"><?php echo $d['styleCSS']; ?></textarea>
+		</p>
+		<p>
+		<label for="<?php echo $this->get_field_id('displayAvatar'); ?>"><input name="<?php echo $this->get_field_name('displayAvatar'); ?>" id="<?php echo $this->get_field_id('displayAvatar'); ?>" type="checkbox" <?php if($d['displayAvatar'] !== 1){ echo 'checked'; } ?> /> Afficher l'avatar des joueurs</label><br />
+		<label for="<?php echo $this->get_field_id('displayCount'); ?>"><input name="<?php echo $this->get_field_name('displayCount'); ?>" id="<?php echo $this->get_field_id('displayCount'); ?>" type="checkbox" <?php if($d['displayCount'] !== 1){ echo 'checked'; } ?> /> Afficher le nombre de joueur en ligne</label>
+		</p>		<p style="border-bottom: 1px dashed #CCCCCC; padding-bottom: 5px;">		Soutenez le créateur de cette extension en vous abonnant à sa chaîne : <a href="http://www.youtube.com/user/PirmaxLePoulpeRouge" target="_blank">PirmaxLePoulpeRouge</a>.		</p>
 		<?php
 
 	}
